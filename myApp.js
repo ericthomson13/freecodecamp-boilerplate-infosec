bd486e2b5bf9e054546ee4b0446eebd14661ed2a
Eric Thomson <et@Erics-MacBook-Pro.local>
2024-08-07 19:40:27 -0600

    Add hsts middleware for https configuration

--- myApp.js
+++ myApp.js
@@ -7,8 +7,8 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
-
+const ninetyDays = 90*24*60*60;
+app.use(helmet.hsts({ maxAge: ninetyDays, force: true }));
 
 
 
